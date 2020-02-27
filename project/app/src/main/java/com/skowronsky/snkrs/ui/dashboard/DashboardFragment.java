d1385f9e0db08256b261ae5886dd112dcc7a0a7d
skowronsky98 <oskar.programming@gmail.com>
2020-02-27 22:08:18 +0100

    mvvm architecture + data binding

--- project/app/src/main/java/com/skowronsky/snkrs/ui/dashboard/DashboardFragment.java
+++ project/app/src/main/java/com/skowronsky/snkrs/ui/dashboard/DashboardFragment.java
@@ -5,31 +5,38 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.skowronsky.snkrs.R;
+import com.skowronsky.snkrs.databinding.FragmentDashboardBinding;
 
 public class DashboardFragment extends Fragment {
 
-    private DashboardViewModel dashboardViewModel;
+    private DashboardViewModel viewModel;
+    private FragmentDashboardBinding binding;
+
+
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-        dashboardViewModel =
-                ViewModelProviders.of(this).get(DashboardViewModel.class);
-        View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
-        final TextView textView = root.findViewById(R.id.text_dashboard);
-        dashboardViewModel.getText().observe(this, new Observer<String>() {
-            @Override
-            public void onChanged(@Nullable String s) {
-                textView.setText(s);
-            }
-        });
-        return root;
+        viewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
+
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashboard, container, false);
+        binding.setLifecycleOwner(this);
+        binding.setDashboardViewModel(viewModel);
+
+
+
+
+
+        return binding.getRoot();
     }
 }
