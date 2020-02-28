d920dcd3d06cf55dbdaef59f062b26e6caab5aff
skowronsky98 <oskar.programming@gmail.com>
2020-02-28 20:56:56 +0100

    data binding + viewModel in mvvm architecture navigation

--- project/app/src/main/java/com/skowronsky/snkrs/ui/dashboard/DashboardFragment.java
+++ project/app/src/main/java/com/skowronsky/snkrs/ui/dashboard/DashboardFragment.java
@@ -1,6 +1,7 @@
 package com.skowronsky.snkrs.ui.dashboard;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,32 +12,50 @@ import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.NavController;
+import androidx.navigation.Navigation;
+import androidx.navigation.fragment.NavHostFragment;
 
 import com.skowronsky.snkrs.R;
 import com.skowronsky.snkrs.databinding.FragmentDashboardBinding;
+
+import java.util.Objects;
 
 public class DashboardFragment extends Fragment {
 
     private DashboardViewModel viewModel;
     private FragmentDashboardBinding binding;
 
-
-
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         viewModel = new ViewModelProvider(this).get(DashboardViewModel.class);
 
         binding = DataBindingUtil.inflate(inflater, R.layout.fragment_dashboard, container, false);
-        binding.setLifecycleOwner(this);
         binding.setDashboardViewModel(viewModel);
+        binding.setLifecycleOwner(this);
 
 
-
+        final LiveData<Boolean> navigation = viewModel.getEventNav();
+        navigation.observe(getViewLifecycleOwner(), new Observer<Boolean>(){
+            @Override
+            public void onChanged(Boolean aBoolean) {
+                if(aBoolean){
+                    Log.i("myTag", "This is my message");
+                    navigateToHome();
+                    viewModel.navFinished();
+                }
+            }
+        });
 
 
         return binding.getRoot();
+    }
+
+    private void navigateToHome(){
+        NavHostFragment.findNavController(this).navigate(R.id.action_navigation_dashboard_to_navigation_home);
     }
 }
--- project/app/src/main/java/com/skowronsky/snkrs/ui/dashboard/DashboardViewModel.java
+++ project/app/src/main/java/com/skowronsky/snkrs/ui/dashboard/DashboardViewModel.java
@@ -1,10 +1,27 @@
 package com.skowronsky.snkrs.ui.dashboard;
 
+import android.util.Log;
+
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 public class DashboardViewModel extends ViewModel {
 
+    private MutableLiveData<Boolean> eventNav;
 
+    public String siema = "siema";
+
+    public MutableLiveData<Boolean> getEventNav(){
+        if(eventNav == null)
+            eventNav = new MutableLiveData<Boolean>();
+        return eventNav;
+    }
+
+    public void setEventNav(){
+        eventNav.setValue(true);
+    }
+    public void navFinished(){
+        eventNav.setValue(false);
+    }
 }
