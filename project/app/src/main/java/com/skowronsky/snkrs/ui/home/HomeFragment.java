5914cc284d200a2c908b8121f41b9c5571a51049
BuildTools <unconfigured@null.spigotmc.org>
2020-04-22 12:55:30 +0200

    adding home fragment fix

--- project/app/src/main/java/com/skowronsky/snkrs/ui/home/HomeFragment.java
+++ project/app/src/main/java/com/skowronsky/snkrs/ui/home/HomeFragment.java
@@ -1,15 +1,11 @@
 package com.skowronsky.snkrs.ui.home;
 
 import android.content.Context;
-import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
-import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.databinding.DataBindingUtil;
@@ -17,7 +13,6 @@ import androidx.fragment.app.Fragment;
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
 import androidx.navigation.fragment.NavHostFragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
@@ -25,12 +20,9 @@ import androidx.recyclerview.widget.RecyclerView;
 import com.skowronsky.snkrs.MyApplication;
 import com.skowronsky.snkrs.R;
 import com.skowronsky.snkrs.RecyclerViewAdapter;
-import com.skowronsky.snkrs.Company;
 import com.skowronsky.snkrs.databinding.FragmentHomeBinding;
 import com.skowronsky.snkrs.model.Brand;
 import com.skowronsky.snkrs.storage.Storage;
-import com.skowronsky.snkrs.ui.dashboard.DashboardFragment;
-import com.skowronsky.snkrs.ui.profile.ProfileViewModelFactory;
 
 import java.util.ArrayList;
 import java.util.Objects;
